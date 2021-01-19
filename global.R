8142fc30b172adb2ffa5ee3f998189b7dccac7d6
ByronLimaMorillo <75829206+ByronLimaMorillo@users.noreply.github.com>
2021-01-18 23:29:17 -0500

    Add files via upload

---
+++ global.R
@@ -0,0 +1,15 @@
+library(DT)
+library(shiny)
+library(shinydashboard)
+library(shinyjs)
+library(readxl)
+library(xlsx)
+library(dplyr)
+library(plotly)
+library(ggplot2)
+library(purrr)
+library(tidyr)
+library(data.table)
+load("datos.RData")
+
+
